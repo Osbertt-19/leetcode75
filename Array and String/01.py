970ffd073ee00ed7c278fd6ad76e0310eadcc75e
Osbertt-19 <minhtetnaing25mhn@gmail.com>
2023-08-20 19:23:37 +0700

    upgraded 01.py and added 02.py

--- Array and String/01.py
+++ Array and String/01.py
@@ -1,13 +1,11 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
         result = ""
-        p1 = 0
-        p2 = 0
-        while p1 < len(word1) and p2 < len(word2):
-            result = result + word1[p1]
-            result = result + word2[p2]
-            p1 = p1 + 1
-            p2 = p2 + 1
+        p = 0
+        while p < len(word1) and p < len(word2):
+            result = result + word1[p]
+            result = result + word2[p]
+            p = p + 1
         print(word1[len(word2) :])
         print(word2[len(word1) :])
         result = result + word1[len(word2) :]
