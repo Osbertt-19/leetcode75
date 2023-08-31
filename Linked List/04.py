36cd7c86d5485a93c46c8b6528282dfdf2bbe9de
Osbertt-19 <minhtetnaing25mhn@gmail.com>
2023-08-31 12:51:30 +0700

    added Linked List

---
+++ Linked List/04.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def pairSum(self, head) -> int:
+        s = []
+        while head:
+            s.append(head.val)
+            head = head.next
+        i = len(s) // 2
+        j = i - 1
+        mx = float("-inf")
+        while j >= 0:
+            sm = s[i] + s[j]
+            if sm > mx:
+                mx = sm
+            i += 1
+            j -= 1
+        return mx
