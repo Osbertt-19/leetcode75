e1a1b564892e2ffb9729b94217e9c81a08e8a428
Osbertt-19 <minhtetnaing25mhn@gmail.com>
2023-09-06 11:38:50 +0700

    added Graphs

--- Graphs - DFS/01.py
+++ Graphs - DFS/01.py
@@ -1,5 +1,18 @@
+from collections import deque
 from typing import List
 
 
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        visited = [False for x in rooms]
+        visited[0] = True
+        keys = rooms[0]
+        while keys:
+            key = keys.pop(0)
+            if not visited[key]:
+                keys += rooms[key]
+                visited[key] = True
+        return all(visited)
+
+
+print(Solution().canVisitAllRooms([[1, 3], [3, 0, 1], [2], [0]]))
